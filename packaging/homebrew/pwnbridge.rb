f723f5bd7abd171e491464e38bd55a9c47edef37
simonfalke <i@simonfalke.com>
2026-07-13 20:41:15 +0800

    Update Homebrew formula to v0.1.8

--- packaging/homebrew/pwnbridge.rb
+++ packaging/homebrew/pwnbridge.rb
@@ -5,15 +5,15 @@
 class Pwnbridge < Formula
   desc "Make a remote Linux x86-64 pwn environment feel local on macOS"
   homepage "https://github.com/simonfalke-01/pwnbridge"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.7/pwnbridge_0.1.7_darwin_arm64.tar.gz"
-    sha256 "1f01e68ccbd78eea1c8e709118e6db10703b3578bf926fdf81a5ec521cc2976e"
+    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.8/pwnbridge_0.1.8_darwin_arm64.tar.gz"
+    sha256 "932c44b94e4cd7df7b549c8ec896a3981bac235c9c4e7a517bcbd4517b2d7434"
   else
-    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.7/pwnbridge_0.1.7_darwin_amd64.tar.gz"
-    sha256 "e776f0e4477de5f85063e850619657d45a4d43ec2ea5f5946cf7166eab2864f4"
+    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.8/pwnbridge_0.1.8_darwin_amd64.tar.gz"
+    sha256 "1a4dd170b2dbc84d18eb9792d073c7d7f425c0788927bd60abd10457b19713af"
   end
 
   depends_on "mutagen-io/mutagen/mutagen"
