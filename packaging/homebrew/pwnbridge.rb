b579be39ea9fc822e5549a3d3516f9c0ac065173
simonfalke <i@simonfalke.com>
2026-07-13 19:45:03 +0800

    Update Homebrew formula to v0.1.7

--- packaging/homebrew/pwnbridge.rb
+++ packaging/homebrew/pwnbridge.rb
@@ -5,21 +5,22 @@
 class Pwnbridge < Formula
   desc "Make a remote Linux x86-64 pwn environment feel local on macOS"
   homepage "https://github.com/simonfalke-01/pwnbridge"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.6/pwnbridge_0.1.6_darwin_arm64.tar.gz"
-    sha256 "828abefbc816770dd764b24448e2774249e986078e0758feea3aeb47470a34ac"
+    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.7/pwnbridge_0.1.7_darwin_arm64.tar.gz"
+    sha256 "1f01e68ccbd78eea1c8e709118e6db10703b3578bf926fdf81a5ec521cc2976e"
   else
-    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.6/pwnbridge_0.1.6_darwin_amd64.tar.gz"
-    sha256 "1332d035f47579f808b15a8093baaf9563fc17d6c8db41def8ac6663e1aac05c"
+    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.1.7/pwnbridge_0.1.7_darwin_amd64.tar.gz"
+    sha256 "e776f0e4477de5f85063e850619657d45a4d43ec2ea5f5946cf7166eab2864f4"
   end
 
   depends_on "mutagen-io/mutagen/mutagen"
 
   def install
     bin.install "pwnbridge"
+    bin.install_symlink "pwnbridge" => "pb"
     (libexec/"pwnbridge").install "pwnbridge-agent-linux-amd64"
     bash_completion.install "completions/pwnbridge.bash" => "pwnbridge"
     zsh_completion.install "completions/_pwnbridge"
@@ -28,6 +29,7 @@ class Pwnbridge < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/pwnbridge version")
+    assert_match "pb COMMAND", shell_output("#{bin}/pb --help")
     assert_path_exists libexec/"pwnbridge/pwnbridge-agent-linux-amd64"
   end
 end
