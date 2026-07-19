0b8706e39163625f1b2c0c8c5f2fbcbb39f97e8d
simonfalke <i@simonfalke.com>
2026-07-19 16:44:00 +0800

    Update Homebrew formula to v0.2.3

--- packaging/homebrew/pwnbridge.rb
+++ packaging/homebrew/pwnbridge.rb
@@ -5,15 +5,15 @@
 class Pwnbridge < Formula
   desc "Make a remote Linux x86-64 pwn environment feel local on macOS"
   homepage "https://github.com/simonfalke-01/pwnbridge"
-  version "0.2.2"
+  version "0.2.3"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.2.2/pwnbridge_0.2.2_darwin_arm64.tar.gz"
-    sha256 "1ffe66aa1c241263cb2109d8ad79a384da807cf9c7934558335d1d18e680819f"
+    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.2.3/pwnbridge_0.2.3_darwin_arm64.tar.gz"
+    sha256 "4b0f678868622ba602101f40b213405fd14ee4e6ac650274241ededfc3c02642"
   else
-    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.2.2/pwnbridge_0.2.2_darwin_amd64.tar.gz"
-    sha256 "f9af16425df1c5acff9d174b4f0fb80bc17a9b86782cf6c7581aafcb4893cdd5"
+    url "https://github.com/simonfalke-01/pwnbridge/releases/download/v0.2.3/pwnbridge_0.2.3_darwin_amd64.tar.gz"
+    sha256 "d2942beb840c5f75f52cfa2ccc3163cd6b56a34e193486feb8d3c06952866b7c"
   end
 
   depends_on "mosh"
